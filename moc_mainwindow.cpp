34fc68d28f0b294e2378da7f8a6c17441f909065
yixiu <2630612613@qq.com>
2024-12-05 23:58:26 +0800

    none

--- moc_mainwindow.cpp
+++ moc_mainwindow.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[31];
-    char stringdata0[491];
+    QByteArrayData data[32];
+    char stringdata0[504];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -37,40 +37,42 @@ QT_MOC_LITERAL(2, 28, 0), // ""
 QT_MOC_LITERAL(3, 29, 4), // "file"
 QT_MOC_LITERAL(4, 34, 4), // "type"
 QT_MOC_LITERAL(5, 39, 14), // "slot_TimerHide"
-QT_MOC_LITERAL(6, 54, 14), // "slot_ShowVideo"
-QT_MOC_LITERAL(7, 69, 3), // "img"
-QT_MOC_LITERAL(8, 73, 17), // "on_pbOpen_clicked"
-QT_MOC_LITERAL(9, 91, 20), // "slot_ReceiveDeration"
-QT_MOC_LITERAL(10, 112, 4), // "time"
-QT_MOC_LITERAL(11, 117, 18), // "slot_ProgressTimer"
-QT_MOC_LITERAL(12, 136, 17), // "slot_StateChanged"
-QT_MOC_LITERAL(13, 154, 18), // "Decoder::PlayState"
-QT_MOC_LITERAL(14, 173, 5), // "state"
-QT_MOC_LITERAL(15, 179, 17), // "on_pbPlay_clicked"
-QT_MOC_LITERAL(16, 197, 17), // "on_pbStop_clicked"
-QT_MOC_LITERAL(17, 215, 24), // "on_hsVolume_valueChanged"
-QT_MOC_LITERAL(18, 240, 5), // "value"
-QT_MOC_LITERAL(19, 246, 27), // "on_hsProgress_sliderPressed"
-QT_MOC_LITERAL(20, 274, 28), // "on_hsProgress_sliderReleased"
-QT_MOC_LITERAL(21, 303, 32), // "on_lwMediaList_itemDoubleClicked"
-QT_MOC_LITERAL(22, 336, 16), // "QListWidgetItem*"
-QT_MOC_LITERAL(23, 353, 4), // "item"
-QT_MOC_LITERAL(24, 358, 17), // "on_pbList_clicked"
-QT_MOC_LITERAL(25, 376, 19), // "on_pbVolume_clicked"
-QT_MOC_LITERAL(26, 396, 21), // "on_pushButton_clicked"
-QT_MOC_LITERAL(27, 418, 30), // "on_cbSpeed_currentIndexChanged"
-QT_MOC_LITERAL(28, 449, 5), // "index"
-QT_MOC_LITERAL(29, 455, 17), // "on_pbNext_clicked"
-QT_MOC_LITERAL(30, 473, 17) // "on_pbPrev_clicked"
+QT_MOC_LITERAL(6, 54, 12), // "slot_TimerPb"
+QT_MOC_LITERAL(7, 67, 14), // "slot_ShowVideo"
+QT_MOC_LITERAL(8, 82, 3), // "img"
+QT_MOC_LITERAL(9, 86, 17), // "on_pbOpen_clicked"
+QT_MOC_LITERAL(10, 104, 20), // "slot_ReceiveDeration"
+QT_MOC_LITERAL(11, 125, 4), // "time"
+QT_MOC_LITERAL(12, 130, 18), // "slot_ProgressTimer"
+QT_MOC_LITERAL(13, 149, 17), // "slot_StateChanged"
+QT_MOC_LITERAL(14, 167, 18), // "Decoder::PlayState"
+QT_MOC_LITERAL(15, 186, 5), // "state"
+QT_MOC_LITERAL(16, 192, 17), // "on_pbPlay_clicked"
+QT_MOC_LITERAL(17, 210, 17), // "on_pbStop_clicked"
+QT_MOC_LITERAL(18, 228, 24), // "on_hsVolume_valueChanged"
+QT_MOC_LITERAL(19, 253, 5), // "value"
+QT_MOC_LITERAL(20, 259, 27), // "on_hsProgress_sliderPressed"
+QT_MOC_LITERAL(21, 287, 28), // "on_hsProgress_sliderReleased"
+QT_MOC_LITERAL(22, 316, 32), // "on_lwMediaList_itemDoubleClicked"
+QT_MOC_LITERAL(23, 349, 16), // "QListWidgetItem*"
+QT_MOC_LITERAL(24, 366, 4), // "item"
+QT_MOC_LITERAL(25, 371, 17), // "on_pbList_clicked"
+QT_MOC_LITERAL(26, 389, 19), // "on_pbVolume_clicked"
+QT_MOC_LITERAL(27, 409, 21), // "on_pushButton_clicked"
+QT_MOC_LITERAL(28, 431, 30), // "on_cbSpeed_currentIndexChanged"
+QT_MOC_LITERAL(29, 462, 5), // "index"
+QT_MOC_LITERAL(30, 468, 17), // "on_pbNext_clicked"
+QT_MOC_LITERAL(31, 486, 17) // "on_pbPrev_clicked"
 
     },
     "MainWindow\0signal_PlayMedia\0\0file\0"
-    "type\0slot_TimerHide\0slot_ShowVideo\0"
-    "img\0on_pbOpen_clicked\0slot_ReceiveDeration\0"
-    "time\0slot_ProgressTimer\0slot_StateChanged\0"
-    "Decoder::PlayState\0state\0on_pbPlay_clicked\0"
-    "on_pbStop_clicked\0on_hsVolume_valueChanged\0"
-    "value\0on_hsProgress_sliderPressed\0"
+    "type\0slot_TimerHide\0slot_TimerPb\0"
+    "slot_ShowVideo\0img\0on_pbOpen_clicked\0"
+    "slot_ReceiveDeration\0time\0slot_ProgressTimer\0"
+    "slot_StateChanged\0Decoder::PlayState\0"
+    "state\0on_pbPlay_clicked\0on_pbStop_clicked\0"
+    "on_hsVolume_valueChanged\0value\0"
+    "on_hsProgress_sliderPressed\0"
     "on_hsProgress_sliderReleased\0"
     "on_lwMediaList_itemDoubleClicked\0"
     "QListWidgetItem*\0item\0on_pbList_clicked\0"
@@ -86,7 +88,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-      19,   14, // methods
+      20,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -94,48 +96,50 @@ static const uint qt_meta_data_MainWindow[] = {
        1,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    2,  109,    2, 0x06 /* Public */,
+       1,    2,  114,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       5,    0,  114,    2, 0x08 /* Private */,
-       6,    1,  115,    2, 0x08 /* Private */,
-       8,    0,  118,    2, 0x08 /* Private */,
-       9,    1,  119,    2, 0x08 /* Private */,
-      11,    0,  122,    2, 0x08 /* Private */,
-      12,    1,  123,    2, 0x08 /* Private */,
-      15,    0,  126,    2, 0x08 /* Private */,
-      16,    0,  127,    2, 0x08 /* Private */,
-      17,    1,  128,    2, 0x08 /* Private */,
-      19,    0,  131,    2, 0x08 /* Private */,
-      20,    0,  132,    2, 0x08 /* Private */,
-      21,    1,  133,    2, 0x08 /* Private */,
-      24,    0,  136,    2, 0x08 /* Private */,
-      25,    0,  137,    2, 0x08 /* Private */,
-      26,    0,  138,    2, 0x08 /* Private */,
-      27,    1,  139,    2, 0x08 /* Private */,
-      29,    0,  142,    2, 0x08 /* Private */,
-      30,    0,  143,    2, 0x08 /* Private */,
+       5,    0,  119,    2, 0x08 /* Private */,
+       6,    0,  120,    2, 0x08 /* Private */,
+       7,    1,  121,    2, 0x08 /* Private */,
+       9,    0,  124,    2, 0x08 /* Private */,
+      10,    1,  125,    2, 0x08 /* Private */,
+      12,    0,  128,    2, 0x08 /* Private */,
+      13,    1,  129,    2, 0x08 /* Private */,
+      16,    0,  132,    2, 0x08 /* Private */,
+      17,    0,  133,    2, 0x08 /* Private */,
+      18,    1,  134,    2, 0x08 /* Private */,
+      20,    0,  137,    2, 0x08 /* Private */,
+      21,    0,  138,    2, 0x08 /* Private */,
+      22,    1,  139,    2, 0x08 /* Private */,
+      25,    0,  142,    2, 0x08 /* Private */,
+      26,    0,  143,    2, 0x08 /* Private */,
+      27,    0,  144,    2, 0x08 /* Private */,
+      28,    1,  145,    2, 0x08 /* Private */,
+      30,    0,  148,    2, 0x08 /* Private */,
+      31,    0,  149,    2, 0x08 /* Private */,
 
  // signals: parameters
     QMetaType::Void, QMetaType::QString, QMetaType::QString,    3,    4,
 
  // slots: parameters
     QMetaType::Void,
-    QMetaType::Void, QMetaType::QImage,    7,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::LongLong,   10,
+    QMetaType::Void, QMetaType::QImage,    8,
     QMetaType::Void,
-    QMetaType::Void, 0x80000000 | 13,   14,
+    QMetaType::Void, QMetaType::LongLong,   11,
     QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void, QMetaType::Int,   18,
+    QMetaType::Void, 0x80000000 | 14,   15,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, 0x80000000 | 22,   23,
+    QMetaType::Void, QMetaType::Int,   19,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 23,   24,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::Int,   28,
+    QMetaType::Void, QMetaType::Int,   29,
     QMetaType::Void,
     QMetaType::Void,
 
@@ -150,23 +154,24 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         switch (_id) {
         case 0: _t->signal_PlayMedia((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2]))); break;
         case 1: _t->slot_TimerHide(); break;
-        case 2: _t->slot_ShowVideo((*reinterpret_cast< QImage(*)>(_a[1]))); break;
-        case 3: _t->on_pbOpen_clicked(); break;
-        case 4: _t->slot_ReceiveDeration((*reinterpret_cast< qint64(*)>(_a[1]))); break;
-        case 5: _t->slot_ProgressTimer(); break;
-        case 6: _t->slot_StateChanged((*reinterpret_cast< Decoder::PlayState(*)>(_a[1]))); break;
-        case 7: _t->on_pbPlay_clicked(); break;
-        case 8: _t->on_pbStop_clicked(); break;
-        case 9: _t->on_hsVolume_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 10: _t->on_hsProgress_sliderPressed(); break;
-        case 11: _t->on_hsProgress_sliderReleased(); break;
-        case 12: _t->on_lwMediaList_itemDoubleClicked((*reinterpret_cast< QListWidgetItem*(*)>(_a[1]))); break;
-        case 13: _t->on_pbList_clicked(); break;
-        case 14: _t->on_pbVolume_clicked(); break;
-        case 15: _t->on_pushButton_clicked(); break;
-        case 16: _t->on_cbSpeed_currentIndexChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 17: _t->on_pbNext_clicked(); break;
-        case 18: _t->on_pbPrev_clicked(); break;
+        case 2: _t->slot_TimerPb(); break;
+        case 3: _t->slot_ShowVideo((*reinterpret_cast< QImage(*)>(_a[1]))); break;
+        case 4: _t->on_pbOpen_clicked(); break;
+        case 5: _t->slot_ReceiveDeration((*reinterpret_cast< qint64(*)>(_a[1]))); break;
+        case 6: _t->slot_ProgressTimer(); break;
+        case 7: _t->slot_StateChanged((*reinterpret_cast< Decoder::PlayState(*)>(_a[1]))); break;
+        case 8: _t->on_pbPlay_clicked(); break;
+        case 9: _t->on_pbStop_clicked(); break;
+        case 10: _t->on_hsVolume_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 11: _t->on_hsProgress_sliderPressed(); break;
+        case 12: _t->on_hsProgress_sliderReleased(); break;
+        case 13: _t->on_lwMediaList_itemDoubleClicked((*reinterpret_cast< QListWidgetItem*(*)>(_a[1]))); break;
+        case 14: _t->on_pbList_clicked(); break;
+        case 15: _t->on_pbVolume_clicked(); break;
+        case 16: _t->on_pushButton_clicked(); break;
+        case 17: _t->on_cbSpeed_currentIndexChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 18: _t->on_pbNext_clicked(); break;
+        case 19: _t->on_pbPrev_clicked(); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -210,13 +215,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 19)
+        if (_id < 20)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 19;
+        _id -= 20;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 19)
+        if (_id < 20)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 19;
+        _id -= 20;
     }
     return _id;
 }
